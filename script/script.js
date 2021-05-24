689558001f77d37173c807673c5d8ed6d884c233
Chris DiPiero <frogmaster99@msn.com>
2021-05-24 10:28:13 -0500

    completed timing function

--- script/script.js
+++ script/script.js
@@ -6,8 +6,10 @@ function setDate() {
   const now = new Date();
 
   const seconds = now.getSeconds();
+  console.log(seconds);
   const secondsDegrees = ((seconds / 60) * 360) + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+
 
   const minutes = now.getMinutes();
   const minutesDegrees = ((minutes / 60) * 360) + 90;
